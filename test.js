1f60d2b2d7dc08187071d4820ec1f7cbb544b8cd
Erin Kavanaugh <erin.kavanaugh@capitalone.com>
2017-04-17 16:56:41 -0700

    initial

---
+++ test.js
@@ -0,0 +1,15 @@
+var axios = require('axios'); 
+var http = require('http')
+
+let   httpsProxyAgent = require('https-proxy-agent');
+// var agent = new httpsProxyAgent(process.env.https_proxy);
+var testApi = 'https://localhost:9000';
+var token = 'token'
+// console.log(agent)
+var config = {
+  baseUrl: testApi,
+  url: '/',
+  httpAgent: agent
+  // headers:  { 'Authorization': "Bearer " + token }
+}
+axios.request(config).then((res) => console.log(res)).catch(err => console.log(err))
